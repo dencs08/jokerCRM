4e3a44104ddac937e2a2322e880dcffad2a77a0e
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-12-18 14:09:14 +0100

    add: Admin layout

---
+++ vue/src/layouts/AdminLayout.vue
@@ -0,0 +1,19 @@
+<template>
+    <router-view></router-view>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+import { Navbar } from '../components'
+
+export default defineComponent({
+    components: {
+        Navbar
+    }
+})
+</script>
+
+<style lang="">
+    
+</style>
