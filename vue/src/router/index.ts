dde71f1084d26c21521841775546f7fcc8eeb8ce
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-12-18 14:09:43 +0100

    add: exports

--- vue/src/router/index.ts
+++ vue/src/router/index.ts
@@ -1,14 +1,26 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-import { DefaultLayout } from "../Layouts";
+import { DefaultLayout, AdminLayout } from "../Layouts";
 
-import { Start } from "../views";
+import { Start, Dashboard, Login, Register } from "../views";
 
 const routes = [
     {
         component: DefaultLayout,
         path: "/start",
-        children: [{ path: "/start", name: "Start", component: Start }],
+        children: [
+            { path: "/start", name: "Start", component: Start },
+            {
+                path: "/login",
+                name: "Login",
+                component: Login,
+            },
+            {
+                path: "/register",
+                name: "Register",
+                component: Register,
+            },
+        ],
     },
 
     {
@@ -16,73 +28,66 @@ const routes = [
         redirect: "/start",
     },
 
-    // {
-    //     component: DefaultLayout,
-    //     path: '/',
-    //     children: [
-    //         { path: '/start', component: Start }
-    //     ]
-    // },
+    {
+        component: AdminLayout,
+        path: "/admin",
+        redirect: "/dashboard",
+        children: [
+            {
+                path: "/dashboard/",
+                component: Dashboard,
+                name: "Dashboard",
+                // redirect: "/dashboard/overview",
+                // beforeEnter: (to, from, next) => {
+                //     axiosAuthenticator
+                //         .get("/api/authenticated")
+                //         .then(() => {
+                //             next();
+                //         })
+                //         .catch(() => {
+                //             return next({ name: "Login" });
+                //         });
+                // },
 
-    // {
-    //     component: AdminLayout,
-    //     path: '/admin',
-    //     redirect: '/dashboard',
-    //     children: [
-    //         {
-    //             path: '/dashboard/',
-    //             component: Dashboard,
-    //             name: 'Dashboard',
-    //             redirect: '/dashboard/overview',
-    //             beforeEnter: (to, from, next) => {
-    //                 axiosAuthenticator.get('/api/authenticated').then(() => {
-    //                     next()
-    //                 }).catch(() => {
-    //                     return next({ name: 'Login' })
-    //                 })
-    //             },
-
-    //             children: [
-    //                 {
-    //                     path: '/dashboard/overview',
-    //                     component: Overview,
-    //                     name: 'Overview'
-    //                 },
-    //                 {
-    //                     path: '/dashboard/emailing',
-    //                     component: Emailing,
-    //                     name: 'Emailing',
-    //                     redirect: '/dashboard/emailing/pricing',
-    //                     children: [{
-    //                         path: '/dashboard/emailing/pricing',
-    //                         component: EmailPricing,
-    //                         name: 'EmailPricing'
-    //                     },
-    //                     {
-    //                         path: '/dashboard/emailing/response',
-    //                         component: EmailResponse,
-    //                         name: 'EmailResponse'
-    //                     }]
-    //                 },
-    //                 {
-    //                     path: '/dashboard/analitics',
-    //                     component: Analitics,
-    //                     name: 'Analitics'
-    //                 },
-    //                 {
-    //                     path: '/dashboard/externallinks',
-    //                     component: ExternalLinks,
-    //                     name: 'ExternalLinks'
-    //                 }
-    //             ]
-    //         },
-    //         {
-    //             path: '/login',
-    //             name: 'Login',
-    //             component: Login,
-    //         }
-    //     ]
-    // }
+                // children:
+                // [
+                //     {
+                //         path: "/dashboard/overview",
+                //         component: Overview,
+                //         name: "Overview",
+                //     },
+                //     {
+                //         path: "/dashboard/emailing",
+                //         component: Emailing,
+                //         name: "Emailing",
+                //         redirect: "/dashboard/emailing/pricing",
+                //         children: [
+                //             {
+                //                 path: "/dashboard/emailing/pricing",
+                //                 component: EmailPricing,
+                //                 name: "EmailPricing",
+                //             },
+                //             {
+                //                 path: "/dashboard/emailing/response",
+                //                 component: EmailResponse,
+                //                 name: "EmailResponse",
+                //             },
+                //         ],
+                //     },
+                //     {
+                //         path: "/dashboard/analitics",
+                //         component: Analitics,
+                //         name: "Analitics",
+                //     },
+                //     {
+                //         path: "/dashboard/externallinks",
+                //         component: ExternalLinks,
+                //         name: "ExternalLinks",
+                //     },
+                // ],
+            },
+        ],
+    },
 ];
 
 const router = createRouter({
--- vue/src/views/index.ts
+++ vue/src/views/index.ts
@@ -1,1 +1,4 @@
 export { default as Start } from "./Start.vue";
+export { default as Login } from "./Login.vue";
+export { default as Register } from "./Register.vue";
+export { default as Dashboard } from "./Admin/Dashboard/Dashboard.vue";
