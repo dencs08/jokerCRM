b39ba6d27b4151dff8d4b1cbfc239bcf5de79275
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-12-18 13:08:55 +0100

    add: Start view

---
+++ vue/src/views/Start.vue
@@ -0,0 +1,15 @@
+<template>
+    <Hero />
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { Hero } from '../containers';
+export default defineComponent({
+    components: {
+        Hero,
+    }
+})
+</script>
+<style lang="scss">
+
+</style>
