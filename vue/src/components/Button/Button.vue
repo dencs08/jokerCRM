dfcbbce1f634d5efdfced9b80b9c1828f8285a1c
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-12-18 13:07:41 +0100

    add: Button component

---
+++ vue/src/components/Button/Button.vue
@@ -0,0 +1,17 @@
+<template>
+    <button class="px-16 py-5 rounded-full bg-mainPink">
+        <slot></slot>
+    </button>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+
+})
+</script>
+<style lang="">
+    props:{
+
+    }
+</style>
