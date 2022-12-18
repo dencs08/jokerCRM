b5c2e12c9a14c940d42428518808b6a44aec633a
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-12-18 14:08:57 +0100

    add: Navbar

---
+++ vue/src/components/Navbar/Navbar.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="fixed">
+        <div class="container mx-auto flex items-center justify-between min-h-[50px] h-[9vh] w-[100vw]">
+            <div>
+                <router-link to="/start">
+                    <img :src="logo" class="min-w-[35px] md:w-[70px]" alt="">
+                </router-link>
+            </div>
+            <div class="space-x-4">
+                <router-link to="/start">Start</router-link>
+                <router-link to="/login">Login</router-link>
+                <router-link to="/register">Register</router-link>
+            </div>
+        </div>
+    </div>
+</template>
+<script lang="ts">
+import logo from '../../assets/logo.svg';
+
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    data() {
+        return {
+            logo: logo,
+        }
+    }
+})
+</script>
+<style lang="">
+    
+</style>
