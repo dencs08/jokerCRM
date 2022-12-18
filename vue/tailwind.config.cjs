a48d8cbfdbd5743c88095757161c51811afa990e
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-12-18 14:10:15 +0100

    update: tailwind.config disable container

--- vue/tailwind.config.cjs
+++ vue/tailwind.config.cjs
@@ -1,5 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  corePlugins: {
+    container: false
+  },
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
