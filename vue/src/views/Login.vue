78e01c862817d0bb0476b2eaa769296356f968bb
dencs08 <59261630+dencs08@users.noreply.github.com>
2023-01-17 13:03:54 +0100

    upadte: Login & Register compoentns

--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -6,12 +6,6 @@
                     <img class="mx-auto h-12 w-auto" src="../assets/logo.svg" alt="Your Company" />
                     <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Sign in to your account
                     </h2>
-                    <p class="mt-2 text-center text-sm text-gray-600">
-                        Or
-                        {{ ' ' }}
-                        <a href="#" class="font-medium text-mainPink">start your 14-day free
-                            trial</a>
-                    </p>
                 </div>
                 <form class="mt-8 space-y-6" action="#" method="POST">
                     <input type="hidden" name="remember" value="true" />
--- vue/src/views/Register.vue
+++ vue/src/views/Register.vue
@@ -6,12 +6,6 @@
                     <img class="mx-auto h-12 w-auto" src="../assets/logo.svg" alt="Your Company" />
                     <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Register new account
                     </h2>
-                    <p class="mt-2 text-center text-sm text-gray-600">
-                        Or
-                        {{ ' ' }}
-                        <a href="#" class="font-medium text-mainPink">start your 14-day free
-                            trial</a>
-                    </p>
                 </div>
                 <form class="mt-8 space-y-6" action="#" method="POST">
                     <input type="hidden" name="remember" value="true" />
@@ -30,11 +24,10 @@
                                 placeholder="Password" />
                         </div>
                         <div>
-                            <label for="password" class="sr-only">Repeat password</label>
-                            <input id="password" name="password" type="password" autocomplete="current-password"
-                                required
+                            <label for="repeat-password" class="sr-only">Repeat password</label>
+                            <input id="repeat-password" name="repeat-password" type="repeat-password" required
                                 class="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                                placeholder="Password" />
+                                placeholder="Repeat Password" />
                         </div>
                     </div>
 
