dd26eeace933da4c412ef459e0b83c76edda504b
dencs08 <59261630+dencs08@users.noreply.github.com>
2023-01-17 13:10:02 +0100

    add: api routers

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,21 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+use App\Http\Controllers\ClientsController;
+use App\Http\Controllers\ContractsController;
+use App\Http\Controllers\AppointmentsController;
+use App\Http\Controllers\SystemUsersController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/login', [LoginController::class, 'login']);
+Route::post('/logout', [LoginController::class, 'logout']);
+
+Route::get('/client/{id}', [ClientsController::class, 'show']);
+Route::delete('/client/{id}', [ClientsController::class, 'destroy']);
+Route::get('/clients', [ClientsController::class, 'index']);
+Route::get('/contracts', [ContractsController::class, 'index']);
+Route::get('/appointments', [AppointmentsController::class, 'index']);
+Route::get('/salesmen', [SystemUsersController::class, 'index']);
