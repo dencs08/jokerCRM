c0292b128e6cac54e0ac93ff3072a2cf84e51db2
dencs08 <59261630+dencs08@users.noreply.github.com>
2023-01-17 13:10:26 +0100

    add: components export

--- vue/src/components/index.ts
+++ vue/src/components/index.ts
@@ -1,2 +1,10 @@
 export { default as Button } from "./Button/Button.vue";
+export { default as InputCustom } from "./InputCustom.vue";
 export { default as Navbar } from "./Navbar/Navbar.vue";
+export { default as ModalUserAdd } from "./ModalUserAdd.vue";
+export { default as ModalClientDisplay } from "./ModalClientDisplay.vue";
+export { default as ModalDanger } from "./ModalDanger.vue";
+export { default as Modal } from "./Modal.vue";
+
+export { default as ClientsTable } from "./ClientsTable.vue";
+export { default as ContractsTable } from "./ContractsTable.vue";
