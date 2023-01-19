89d213b4b9b6aa70da5ac007882b2235d06d1b64
dencs08 <59261630+dencs08@users.noreply.github.com>
2023-01-19 23:17:34 +0100

    add: Client model relations

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -8,6 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Client extends Model
 {
     use HasFactory;
-
+    protected $fillable = [
+        'name', 'ceoname', 'email', 'phone', 'nip', 'city', 'zipcode',
+    ];
     public $timestamps = false;
+    public function appointments()
+    {
+        return $this->hasMany(Appointment::class);
+    }
+    public function contracts()
+    {
+        return $this->hasMany(Contract::class);
+    }
+
+    public function emails()
+    {
+        return $this->hasMany(Email::class);
+    }
 }
