2f35e56955c3126fe72965868cc262dc5a01a569
dencs08 <59261630+dencs08@users.noreply.github.com>
2023-01-19 21:15:32 +0100

    add: AppointmentsTable in Appointments view

--- vue/src/views/Admin/Appointments.vue
+++ vue/src/views/Admin/Appointments.vue
@@ -1,11 +1,15 @@
 <template>
     <div>
         Appointments
+        <AppointmentsTable />
     </div>
 </template>
 <script lang="ts">
+import { AppointmentsTable } from '../../components'
 export default {
-
+    components: {
+        AppointmentsTable
+    }
 }
 </script>
 <style lang="">
