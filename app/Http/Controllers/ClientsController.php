0110fa75b67ebe94c391daab523e1b0d2498353e
dencs08 <59261630+dencs08@users.noreply.github.com>
2023-01-19 23:18:23 +0100

    add: ClientsController index, store, show

--- app/Http/Controllers/ClientsController.php
+++ app/Http/Controllers/ClientsController.php
@@ -9,72 +9,54 @@ class ClientsController extends Controller
 {
     public function index()
     {
-        $clients = Client::all();
+        $clients = Client::with(['appointments', 'contracts','emails'])->get();
         return $clients;
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
+        $validatedData = $request->validate([
+            'name' => 'required',
+            'ceoname' => 'required',
+            'email' => 'required',
+            'phone' => 'required',
+            'nip' => 'required',
+            'city' => 'required',
+            'zipcode' => 'required',
+        ]);
 
+        $client = Client::create([
+            'name' => $request['name'],
+            'ceoname' => $request['ceoname'],
+            'email' => $request['email'],
+            'phone' => $request['phone'],
+            'nip' => $request['nip'],
+            'city' => $request['city'],
+            'zipcode' => $request['zipcode'],
+        ]);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
-        $client = Client::find($id);
+        $client = Client::with(['appointments', 'contracts','emails'])->find($id);
         return $client;
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
         $user = Client::findOrFail($id);
