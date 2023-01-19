9e0aa4ad8b635105b81e79297ef315f2a5562d6f
dencs08 <59261630+dencs08@users.noreply.github.com>
2023-01-19 23:19:08 +0100

    update: cors & sanctum cors policies

--- config/cors.php
+++ config/cors.php
@@ -15,11 +15,11 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', '/api/*' ,'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['http://localhost:8000', 'https://danielbuzon.pl','https://api.danielbuzon.pl', 'http://localhost:5173'],
 
     'allowed_origins_patterns' => [],
 
@@ -29,6 +29,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
