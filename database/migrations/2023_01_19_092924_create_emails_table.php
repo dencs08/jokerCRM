66200dcb3073b7542e960b9f5633d461ff9c0c48
dencs08 <59261630+dencs08@users.noreply.github.com>
2023-01-19 21:14:45 +0100

    add: emails migration

---
+++ database/migrations/2023_01_19_092924_create_emails_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('emails', function (Blueprint $table) {
+            $table->id();
+            $table->string('message');
+            $table->string('subject');
+            $table->unsignedBigInteger('client_id')->nullable();
+            $table->unsignedBigInteger('system_user_id')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('emails');
+    }
+};
