bd0a20b81e2b1f71d249ec9eb431e00f93eb75fb
dencs08 <59261630+dencs08@users.noreply.github.com>
2023-01-20 10:57:46 +0100

    add; InputCustom is now bindable with v-model

--- vue/src/components/InputCustom.vue
+++ vue/src/components/InputCustom.vue
@@ -6,9 +6,9 @@
             <span v-if="optional" class="text-sm text-gray-500" :id="name + '-optional'">Opcjonalne</span>
         </div>
         <div class="mt-1">
-            <input :type="type" :name="name" :id="name"
+            <input :type="type" :name="name" :id="name" v-model="value"
                 class="block w-full rounded-md border border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 text-xs sm:text-sm p-2 sm:p-3"
-                :placeholder="placeholder" :aria-describedby="name + '-optional'" />
+                :placeholder="placeholder" :aria-describedby="name + '-optional'" :required="required" />
         </div>
     </div>
 </template>
@@ -22,11 +22,30 @@ export default {
         placeholder: String,
         required: Boolean,
 
+        modelValue: String,
+
         optional: {
             default: false,
             type: Boolean,
         }
-    }
+    },
+
+    data() {
+        return {
+            content: this.value
+        }
+    },
+
+    computed: {
+        value: {
+            get() {
+                return this.modelValue
+            },
+            set(value: any) {
+                this.$emit('update:modelValue', value)
+            }
+        }
+    },
 }
 </script>
 <style lang="">
