a02c65cb72b257b49bd1a50eec8dd43ba869aebd
dencs08 <59261630+dencs08@users.noreply.github.com>
2023-01-20 10:58:42 +0100

    add: export Salesman view

--- vue/src/views/index.ts
+++ vue/src/views/index.ts
@@ -7,6 +7,7 @@ export { default as Clients } from "./Admin/Clients.vue";
 export { default as Client } from "./Admin/Client.vue";
 export { default as Emails } from "./Admin/Emails.vue";
 export { default as Salesmans } from "./Admin/Salesmans.vue";
+export { default as Salesman } from "./Admin/Salesman.vue";
 export { default as Appointments } from "./Admin/Appointments.vue";
 export { default as MyAppointments } from "./Admin/MyAppointments.vue";
 export { default as Contracts } from "./Admin/Contracts.vue";
