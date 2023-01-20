d749a3eae634637f5e4203c9f590ef9c221955a6
dencs08 <59261630+dencs08@users.noreply.github.com>
2023-01-20 10:56:06 +0100

    add: ContractsTable essenital view & links

--- vue/src/components/ContractsTable.vue
+++ vue/src/components/ContractsTable.vue
@@ -9,24 +9,30 @@
                                 <th scope="col"
                                     class="whitespace-nowrap py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">
                                     Transaction ID</th>
-                                <th v-if="user.role == 'Admin'" scope="col"
+                                <th v-if="user.role == 'Admin' && !essential" scope="col"
                                     class="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900">
-                                    Salesman</th>
+                                    Salesman
+                                </th>
                                 <th scope="col"
                                     class="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900">
-                                    Company</th>
+                                    Company
+                                </th>
+                                <th v-if="!essential" scope="col"
+                                    class="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900">
+                                    Commision
+                                </th>
+                                <th v-if="!essential" scope="col"
+                                    class="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900">
+                                    Price
+                                </th>
                                 <th scope="col"
                                     class="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900">
-                                    Commision</th>
+                                    Net amount
+                                </th>
                                 <th scope="col"
                                     class="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900">
-                                    Price</th>
-                                <th scope="col"
-                                    class="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900">
-                                    Net amount</th>
-                                <th scope="col"
-                                    class="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900">
-                                    Status</th>
+                                    Status
+                                </th>
                                 <th scope="col"
                                     class="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900">
                                     <span class="sr-only">Actions</span>
@@ -34,36 +40,56 @@
                             </tr>
                         </thead>
                         <tbody class="divide-y divide-gray-200 bg-white">
-                            <tr v-for="transaction in transactions" :key="transaction.id">
-                                <td class="whitespace-nowrap py-2 pl-4 pr-3 text-sm text-gray-500 sm:pl-6">{{
-                                    transaction.id
-                                }}</td>
-                                <td v-if="user.role == 'Admin'"
-                                    class="whitespace-nowrap px-2 py-2 text-sm font-medium text-gray-900">{{
-                                        transaction.salesman
-                                    }}</td>
-                                <td class="whitespace-nowrap px-2 py-2 text-sm text-gray-900">{{
-                                    transaction.client
-                                }}</td>
-                                <td class="whitespace-nowrap px-2 py-2 text-sm text-gray-500">{{
-                                    transaction.commission
-                                }}</td>
-                                <td class="whitespace-nowrap px-2 py-2 text-sm text-gray-500">{{
-                                    transaction.price
-                                }}</td>
-                                <td class="whitespace-nowrap px-2 py-2 text-sm text-gray-500">{{
-                                    transaction.netAmount
-                                }}</td>
+                            <tr v-for="contract in contracts" :key="contract.id">
+                                <td class="whitespace-nowrap py-2 pl-4 pr-3 text-sm text-gray-500 sm:pl-6">
+                                    {{
+                                        contract.transactionID
+                                    }}
+                                </td>
+                                <td v-if="user.role == 'Admin' && !essential"
+                                    class="whitespace-nowrap px-2 py-2 text-sm font-medium text-gray-900">
+                                    <router-link
+                                        :to="{ name: 'Salesman', params: { id: contract.system_user.id.toString() } }"
+                                        class="text-indigo-600 hover:text-indigo-900">
+                                        {{ contract.system_user.name }}
+                                    </router-link>
+                                </td>
+                                <td class="whitespace-nowrap px-2 py-2 text-sm text-gray-900">
+                                    <router-link :to="{ name: 'Client', params: { id: contract.client.id.toString() } }"
+                                        class="text-indigo-600 hover:text-indigo-900">
+                                        {{ contract.client.name }}
+                                    </router-link>
+                                </td>
+                                <td v-if="!essential" class="whitespace-nowrap px-2 py-2 text-sm text-gray-500">
+                                    {{
+                                        contract.commision
+                                    }}
+                                </td>
+                                <td v-if="!essential" class="whitespace-nowrap px-2 py-2 text-sm text-gray-500">
+                                    {{
+                                        contract.price
+                                    }}
+                                </td>
                                 <td class="whitespace-nowrap px-2 py-2 text-sm text-gray-500">
-                                    <span class="rounded-full px-3 py-1 text-xs text-white" :class="[transaction.status === 'Completed' ? 'bg-emerald-500' : '',
-                                    transaction.status === 'Canceled' ? 'bg-red-500' : '',
-                                    transaction.status === 'Pending' ? 'bg-yellow-500' : '']">
-                                        {{ transaction.status }}
+                                    {{
+                                        contract.netamount
+                                    }}
+                                </td>
+                                <td class="whitespace-nowrap px-2 py-2 text-sm text-gray-500">
+                                    <span class="rounded-full px-3 py-1 text-xs text-white" :class="[contract.status === 'Completed' ? 'bg-emerald-500' : '',
+                                    contract.status === 'Canceled' ? 'bg-red-500' : '',
+                                    contract.status === 'Pending' ? 'bg-yellow-500' : '']">
+                                        {{ contract.status }}
                                     </span>
                                 </td>
                                 <td
                                     class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6 space-x-4">
-                                    <a href="#" class="text-indigo-600 hover:text-indigo-900">Remove</a>
+                                    <a href="#" @click="destroyContractID(contract.id)"
+                                        class="text-indigo-600 hover:text-indigo-900">
+                                        <span>
+                                            <Icon icon="bxs:trash" class="inline" />
+                                        </span>
+                                    </a>
                                 </td>
                             </tr>
                         </tbody>
@@ -74,41 +100,96 @@
     </div>
 </template>
 <script lang="ts">
+import { getContracts, getClient, getSalesman, destroyContract } from '../ts/axios';
+import { Icon } from '@iconify/vue'
+import { nextTick } from 'vue'
+
 export default {
+    components: { Icon },
+    props: {
+        id: Number as any,
+        essential: {
+            type: Boolean,
+            default: false
+        }
+    },
     data() {
         return {
             user: {
+                id: 10,
                 role: 'Admin'
             },
-            transactions: [
-                {
-                    id: 'FV2023/1',
-                    salesman: 'Sprzedawca 1',
-                    client: 'Klient 1',
-                    commission: '+$50',
+            client: {
+                id: 1,
+                name: '',
+                contracts: [{
+                    id: '1',
+                    transactionID: 'FV2023/1',
+                    commision: '+$50',
                     price: '$3,509.00',
                     status: 'Completed',
-                    netAmount: '$4,397.00',
-                },
+                    netamount: '$4,397.00',
+                    client: {
+                        id: 1,
+                        name: ''
+                    },
+                    system_user: {
+                        id: 1,
+                        name: ''
+                    }
+                }]
+            },
+            contracts: [
                 {
-                    id: 'FV2023/2',
-                    salesman: 'Sprzedawca 2',
-                    client: 'Klient 2',
-                    commission: '+$50',
+                    id: '1',
+                    transactionID: 'FV2023/1',
+                    commision: '+$50',
                     price: '$3,509.00',
-                    status: 'Pending',
-                    netAmount: '$4,397.00',
-                },
-                {
-                    id: 'FV2023/3',
-                    salesman: 'Sprzedawca 3',
-                    client: 'Klient 2',
-                    commission: '+$50',
-                    price: '$3,509.00',
-                    status: 'Canceled',
-                    netAmount: '$4,397.00',
+                    status: 'Completed',
+                    netamount: '$4,397.00',
+                    client: {
+                        id: 1,
+                        name: ''
+                    },
+                    system_user: {
+                        id: 1,
+                        name: ''
+                    }
                 },
             ],
+        }
+    },
+
+    async mounted() {
+        this.getData()
+    },
+
+    methods: {
+        async destroyContractID(id: any) {
+            await destroyContract(id);
+            await nextTick()
+            setTimeout(async () => {
+                this.getData()
+            }, 500);
+        },
+        async getData() {
+            if (this.$route.name === "MyContracts") {
+                this.contracts = await getContracts(this.user.id);
+                return;
+            }
+            if (this.$route.name === "Client") {
+                this.client = await getClient(this.id);
+                this.contracts = this.client.contracts;
+                return;
+            }
+            if (this.$route.name === "Salesman") {
+                this.client = await getSalesman(this.id);
+                this.contracts = this.client.contracts;
+                return;
+            }
+
+            this.contracts = await getContracts(this.user.id);
+            if (this.essential) this.contracts.length = 10;
         }
     }
 }
