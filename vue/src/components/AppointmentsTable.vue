c2edc209bdb7c7f8d45d4170d8ac766ceb77d3ff
dencs08 <59261630+dencs08@users.noreply.github.com>
2023-01-20 10:55:57 +0100

    add: AppointmentsTable essenital view & links

--- vue/src/components/AppointmentsTable.vue
+++ vue/src/components/AppointmentsTable.vue
@@ -8,7 +8,11 @@
                             <tr>
                                 <th scope="col"
                                     class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">
-                                    Name</th>
+                                    Client</th>
+                                <th v-if="user.role == 'Admin' && !essential" scope="col"
+                                    class="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900">
+                                    Salesman
+                                </th>
                                 <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
                                     Location</th>
                                 <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
@@ -16,18 +20,29 @@
                                 <th scope="col" v-if="!essential"
                                     class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
                                     Time</th>
+                                <th v-if="!essential" scope="col"
+                                    class="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900">
+                                    Status
+                                </th>
                                 <th scope="col" class="relative py-3.5 pl-1 pr-1 sm:pr-0">
                                     <span class="sr-only">Actions</span>
                                 </th>
                             </tr>
                         </thead>
                         <tbody class="divide-y divide-gray-200 bg-white">
-                            <tr v-for="meeting in meetings" :key="meeting.id">
+                            <tr v-for="meeting in appointments" :key="meeting.id">
                                 <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
                                     <router-link :to="{ name: 'Client', params: { id: meeting.client.id.toString() } }"
                                         class="text-indigo-600 hover:text-indigo-900">
-                                        <!-- :to="{ name: 'Client', params: { id: meeting.client.id.toString() } -->
                                         {{ meeting.client.name }}
+                                    </router-link>
+                                </td>
+                                <td v-if="user.role == 'Admin' && !essential"
+                                    class="whitespace-nowrap px-2 py-2 text-sm font-medium text-gray-900">
+                                    <router-link
+                                        :to="{ name: 'Salesman', params: { id: meeting.system_user.id.toString() } }"
+                                        class="text-indigo-600 hover:text-indigo-900">
+                                        {{ meeting.system_user.name }}
                                     </router-link>
                                 </td>
                                 <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ meeting.location }}
@@ -37,6 +52,14 @@
                                 <td v-if="!essential" class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{
                                     meeting.time
                                 }}</td>
+                                <td v-if="!essential" class="whitespace-nowrap px-2 py-2 text-sm text-gray-500">
+                                    <span class="rounded-full px-3 py-1 text-xs text-white" :class="[meeting.status === 'Completed' ? 'bg-emerald-500' : '',
+                                    meeting.status === 'Canceled' ? 'bg-red-500' : '',
+                                    meeting.status === 'Confirmed' ? 'bg-blue-500' : '',
+                                    meeting.status === 'Unconfirmed' ? 'bg-yellow-500' : '']">
+                                        {{ meeting.status }}
+                                    </span>
+                                </td>
                                 <td
                                     class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6 space-x-4">
                                     <router-link :to="{ name: 'Client', params: { id: meeting.client.id.toString() } }"
@@ -47,7 +70,8 @@
                                         </span>
                                     </router-link>
 
-                                    <a href="#" @click="" class="text-indigo-600 hover:text-indigo-900">
+                                    <a href="#" @click="destroyAppointmentID(meeting.id)"
+                                        class="text-indigo-600 hover:text-indigo-900">
                                         <span>
                                             <Icon icon="bxs:trash" class="inline" />
                                         </span>
@@ -63,14 +87,14 @@
 </template>
 <script lang="ts">
 import { Icon } from '@iconify/vue'
-import { axiosClient } from '../ts/axios'
-import axios from 'axios'
+import { getAppointments, getClient, getSalesman, destroyAppointment } from '../ts/axios'
+import { nextTick } from 'vue'
+
 export default {
     components: {
         Icon
     },
     props: {
-        get: String,
         id: Number as any,
 
         essential: {
@@ -81,15 +105,45 @@ export default {
 
     data() {
         return {
-            userid: 10,
-            meetings: [
+            user: {
+                id: 10,
+                role: 'Admin'
+            },
+            client: {
+                id: 1,
+                name: '',
+                appointments: [
+                    {
+                        id: 1,
+                        location: 'Location 2',
+                        date: '10 January 2022',
+                        time: '16:00',
+                        datetime: '2022-01-10T17:00',
+                        status: '',
+                        client: {
+                            id: 1,
+                            name: ''
+                        },
+                        system_user: {
+                            id: 1,
+                            name: ''
+                        }
+                    }
+                ]
+            },
+            appointments: [
                 {
                     id: 1,
                     location: 'Location 2',
                     date: '10 January 2022',
                     time: '16:00',
                     datetime: '2022-01-10T17:00',
+                    status: '',
                     client: {
+                        id: 1,
+                        name: ''
+                    },
+                    system_user: {
                         id: 1,
                         name: ''
                     }
@@ -99,18 +153,37 @@ export default {
     },
 
     async mounted() {
-        this.meetings = await this.getAppointments();
+        this.getData();
     },
 
     methods: {
-        async getAppointments() {
-            try {
-                const response = await axiosClient.get("/appointments/" + this.userid);
-                return response.data;
-            } catch (error) {
-                console.log(error);
-                return error;
+        async destroyAppointmentID(id: any) {
+            await destroyAppointment(id);
+            await nextTick()
+            setTimeout(async () => {
+                this.getData()
+            }, 500);
+        },
+        async getData() {
+            if (this.$route.name === "MyAppointments") {
+                this.appointments = await getAppointments(this.user.id);
+                return;
             }
+            if (this.$route.name === "Client") {
+                this.client = await getClient(this.id);
+                this.appointments = this.client.appointments;
+                return;
+            }
+            if (this.$route.name === "Salesman") {
+                this.client = await getSalesman(this.id);
+                this.appointments = this.client.appointments;
+                return;
+            }
+
+            this.appointments = await getAppointments(this.user.id);
+            console.log(this.appointments);
+
+            if (this.essential) this.appointments.length = 10;
         }
     }
 }
