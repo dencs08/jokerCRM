7cb1482b55702fc131c6355049f84673b7cb6a6b
dencs08 <59261630+dencs08@users.noreply.github.com>
2023-01-20 10:54:22 +0100

    add: salesmen, salesman, & destroy appointment contract api routes

--- routes/api.php
+++ routes/api.php
@@ -18,12 +18,17 @@ Route::get('/client/{id}', [ClientsController::class, 'show']);
 Route::delete('/client/{id}', [ClientsController::class, 'destroy']);
 Route::post('/client/add', [ClientsController::class, 'store']);
 
-Route::get('/clients', [ClientsController::class, 'index']);
+Route::get('/clients/{id}', [ClientsController::class, 'index']);
+
+Route::get('/salesmen', [SystemusersController::class, 'index']);
+Route::get('/salesman/{id}', [SystemusersController::class, 'show']);
 
 Route::get('/appointments/{id}', [AppointmentsController::class, 'show']);
+Route::delete('/appointment/{id}', [AppointmentsController::class, 'destroy']);
 
 Route::get('/contracts/{id}', [ContractsController::class, 'show']);
 Route::get('/contracts', [ContractsController::class, 'index']);
+Route::delete('/contract/{id}', [ContractsController::class, 'destroy']);
 
 Route::get('/appointments', [AppointmentsController::class, 'index']);
 Route::get('/salesmen', [SystemUsersController::class, 'index']);
