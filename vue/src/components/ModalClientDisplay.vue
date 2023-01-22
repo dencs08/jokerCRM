520ac4e6183a06fbd0db4fb4b5a67a8f1983b25b
dencs08 <59261630+dencs08@users.noreply.github.com>
2023-01-22 23:39:34 +0100

    update: title in ModalClientDisplay

--- vue/src/components/ModalClientDisplay.vue
+++ vue/src/components/ModalClientDisplay.vue
@@ -1,17 +1,26 @@
 <template>
     <div class="bg-gray-50 px-5 py-4 flex justify-between items-center">
-        <p> Client specific statistics</p>
+        <p> Clients additional information</p>
         <Icon @click="$emit('toggleModal')" icon="ic:twotone-close" width="15px" class="cursor-pointer" />
     </div>
 
     <div class="divide-y">
         <div class="px-3 py-3">
             <div class="grid grid-cols-1 [&>*]:mr-6 [&>*]:my-2">
-                <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-5">
-                    <div v-for="item in stats" :key="item.name"
+                <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-1">
+                    <div class="flex justify-between">
+                        <p class="text-md font-medium text-gray-800">Client name: {{ client.name }}</p>
+                        <!-- <button class="text-xs font-medium px-3 py-2 rounded-md text-white bg-purple-500">Add</button> -->
+                    </div>
+                    <div v-for="info in client.client_info"
                         class="overflow-hidden rounded-lg bg-white px-4 py-5 shadow border sm:p-6 min-h-[125px] flex flex-col justify-center">
-                        <dt class="truncate text-sm font-medium text-gray-500">{{ item.name }}</dt>
-                        <dd class="mt-1 text-3xl font-semibold tracking-tight text-gray-900">{{ item.stat }}</dd>
+                        <dt class="truncate text-md font-medium text-gray-800">{{ info.title }}</dt>
+                        <dd class="mt-1 text-sm font-regular tracking-tight text-gray-500">
+                            {{ info.info }}
+                        </dd>
+
+                        <a href="#" @click="infoDestroyID(info.id)"
+                            class="font-semibold text-xs text-purple-500 mt-2">Remove</a>
                     </div>
                 </dl>
             </div>
@@ -31,17 +40,34 @@
         </div> -->
 </template>
 <script lang="ts">
+import { getClient, infoDestroy } from '../ts/axios'
+import { Icon } from '@iconify/vue'
 export default {
+    components: { Icon },
     data() {
         return {
-            stats: [
-                { name: 'All sales', stat: '71,897' },
-                { name: 'Avg. sales', stat: '3000,11' },
-                { name: 'Clients', stat: '30' },
-                { name: 'Contracts completed', stat: '10' },
-                { name: 'Emails sent', stat: '59' },
-                { name: 'Appointments completed', stat: '129' },
-            ]
+            client: {
+                id: 0,
+                name: '',
+                client_info: [{
+                    id: 0,
+                    title: '',
+                    info: ''
+                }]
+            }
+        }
+    },
+
+    async mounted() {
+        this.client = await getClient(this.$route.params.id);
+    },
+
+    methods: {
+        async infoDestroyID(id: any) {
+            infoDestroy(id);
+            setTimeout(async () => {
+                this.client = await getClient(this.$route.params.id);
+            }, 500);
         }
     }
 }
