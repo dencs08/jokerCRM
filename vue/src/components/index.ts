07d64e897fefba0c8baf7820f03fbbaf112ef653
dencs08 <59261630+dencs08@users.noreply.github.com>
2023-01-22 23:40:18 +0100

    add: export new modal components

--- vue/src/components/index.ts
+++ vue/src/components/index.ts
@@ -1,9 +1,14 @@
 export { default as Button } from "./Button/Button.vue";
 export { default as InputCustom } from "./InputCustom.vue";
+export { default as ListBox } from "./ListBox.vue";
 export { default as Navbar } from "./Navbar/Navbar.vue";
 export { default as ModalUserAdd } from "./ModalUserAdd.vue";
 export { default as ModalClientDisplay } from "./ModalClientDisplay.vue";
 export { default as ModalDanger } from "./ModalDanger.vue";
+export { default as ModalContractAdd } from "./ModalContractAdd.vue";
+export { default as ModalAppointmentAdd } from "./ModalAppointmentAdd.vue";
+export { default as ModalContactPersonAdd } from "./ModalContactPersonAdd.vue";
+export { default as ModalInfoAdd } from "./ModalInfoAdd.vue";
 export { default as Modal } from "./Modal.vue";
 
 export { default as ClientsTable } from "./ClientsTable.vue";
