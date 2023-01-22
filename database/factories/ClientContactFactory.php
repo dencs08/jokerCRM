9698ad8942a6ccee19c7ab6e54f477645084a9a6
dencs08 <59261630+dencs08@users.noreply.github.com>
2023-01-22 23:38:15 +0100

    add: ClientContact ClientInfo factory

---
+++ database/factories/ClientContactFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class ClientContactFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
+            'phone' => fake()->bothify('###-###-###'),
+            'client_id' => fake()->numberBetween($min = 1, $max = 10),
+        ];
+    }
+}
