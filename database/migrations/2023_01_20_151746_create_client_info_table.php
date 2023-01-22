ea17d0b455df1694523de8c02a19487c342ceae0
dencs08 <59261630+dencs08@users.noreply.github.com>
2023-01-22 23:37:24 +0100

    add: Client Info, Client contact migrations

---
+++ database/migrations/2023_01_20_151746_create_client_info_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('client_infos', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('info');
+            $table->unsignedBigInteger('client_id')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('client_infos');
+    }
+};
