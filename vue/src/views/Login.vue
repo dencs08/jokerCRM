4af9b748c4136826dce3ed99f63de2494a078bcf
dencs08 <59261630+dencs08@users.noreply.github.com>
2023-01-23 12:22:55 +0100

    add: HandleLogin function in Login

--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -7,19 +7,20 @@
                     <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Sign in to your account
                     </h2>
                 </div>
-                <form class="mt-8 space-y-6" action="#" method="POST">
+                <form class="mt-8 space-y-6" action="#" @submit.prevent="handleLogin">
                     <input type="hidden" name="remember" value="true" />
                     <div class="-space-y-px rounded-md shadow-sm">
                         <div>
-                            <label for="email-address" class="sr-only">Email address</label>
-                            <input id="email-address" name="email" type="email" autocomplete="email" required
+                            <label for="name" class="sr-only">Name</label>
+                            <input v-model="formData.name" id="name" name="name" type="name" autocomplete="name"
+                                required
                                 class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                                placeholder="Email address" />
+                                placeholder="Name" />
                         </div>
                         <div>
                             <label for="password" class="sr-only">Password</label>
-                            <input id="password" name="password" type="password" autocomplete="current-password"
-                                required
+                            <input v-model="formData.password" id="password" name="password" type="password"
+                                autocomplete="current-password" required
                                 class="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
                                 placeholder="Password" />
                         </div>
@@ -55,11 +56,43 @@
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue';
-
+import { axiosAuthenticator } from '../ts/axios'
 
 import { LockClosedIcon } from '@heroicons/vue/20/solid'
 
 export default defineComponent({
+    components: {
+        LockClosedIcon
+    },
+    data() {
+        return {
+            error: '',
+            formData: {
+                name: '',
+                password: ''
+            }
+        }
+    },
+
+    mounted() {
+    },
+
+    methods: {
+        handleLogin() {
+            axiosAuthenticator.get('/sanctum/csrf-cookie').then(response => {
+                console.log(response);
+                axiosAuthenticator.post('/api/login', this.formData)
+                    .then(response => {
+                        this.$router.push({ path: '/dashboard' })
+                        console.log(response);
+                    })
+                    .catch(error => {
+                        this.error = 'Invalid name or password.';
+                        console.log(response);
+                    })
+            });
+        },
+    }
 })
 </script>
 <style lang="scss">
