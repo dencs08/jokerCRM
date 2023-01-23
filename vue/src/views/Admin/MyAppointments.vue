e33860a304d489911c87c3df993b61e614e33e72
dencs08 <59261630+dencs08@users.noreply.github.com>
2023-01-23 12:22:29 +0100

    update: commented add button in MyAppointments & MyContracts

--- vue/src/views/Admin/MyAppointments.vue
+++ vue/src/views/Admin/MyAppointments.vue
@@ -4,11 +4,11 @@
             <div class="sm:flex-auto">
                 <h1 class="text-xl font-semibold text-gray-900">My Appointments</h1>
             </div>
-            <div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
+            <!-- <div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
                 <button type="button"
                     class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto">Add
                     appointment</button>
-            </div>
+            </div> -->
         </div>
         <AppointmentsTable />
     </div>
